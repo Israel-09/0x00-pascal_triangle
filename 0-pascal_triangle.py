721ac8983b02cb7b7529a5456c8f8ad9abae07b5
israel-09 <israelinene91@gmail.com>
2024-03-09 23:37:55 +0100

    pascal triangle solution

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+pascal triangle algorithm
+"""
+
+def pascal_triangle(n):
+    #initalize loop till n
+    
+    if type(n) is not int or n < 0:
+        raise TypeError("n must be an integer not less than 0")
+
+    triangle = [[1]]
+
+    for i in range(1, n + 1):
+        row = [1]
+        for k in range(1, i):
+            value = triangle[i - 1][k] + triangle[i - 1][k - 1]
+            row.append(value)
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
