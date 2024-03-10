a33bbcea98a469fdc31e798d9a2d6496b357abca
israel-09 <israelinene91@gmail.com>
2024-03-10 19:04:16 +0100

    update the pascal triangle algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,22 +3,30 @@
 pascal triangle algorithm
 """
 
+
+def get_next_row(prev):
+    """
+    helper function to generate next row
+    """
+    row = [1]
+    for i in range(1, len(prev)):
+        row.append(prev[i] + prev[i - 1])
+    row.append(1)
+    return row
+
+
 def pascal_triangle(n):
     """
     pascal triangle generator
     """
-    
+
     if type(n) is not int or n < 0:
         raise TypeError("n must be an integer not less than 0")
 
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for k in range(1, i):
-            value = triangle[i - 1][k] + triangle[i - 1][k - 1]
-            row.append(value)
-        row.append(1)
+    triangle = []
+    row = [1]
+    for i in range(n):
         triangle.append(row)
+        row = get_next_row(row)
 
     return triangle
