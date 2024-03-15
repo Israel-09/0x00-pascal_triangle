cf367a124d831963018b66e606c595cbf0ed1a57
israel-09 <israelinene91@gmail.com>
2024-03-15 05:54:41 +0100

    solution lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+lockboxes algorithm
+"""
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """
+    tries to unlock all box in boxes
+
+    Args:
+        boxes (list): box containing all boxes
+    Return:
+        True if all boxes are opened. Otherwise, False
+    """
+    if not boxes:
+        return False
+    
+    n = len(boxes)
+    visited = set()
+    queue = deque([0])
+    
+    while queue:
+        box = queue.popleft()
+        visited.add(box)
+        
+        for key in boxes[box]:
+            if key < n and key not in visited:
+                queue.append(key)
+    
+    return len(visited) == n
