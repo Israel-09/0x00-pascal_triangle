f349e1b27fffbff936e7cf9f8cc568b4c4644cd4
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2024-05-09 20:27:53 +0100

    Refactor find_exposed_sides function to handle edge cases in island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
     solution to island perimeter problem
 """
 
+
 def find_exposed_sides(pos, grid):
     """_summary_
 
@@ -11,13 +12,14 @@ def find_exposed_sides(pos, grid):
         col (_type_): _description_
     """
     x, y = pos
-    try:
-        top = grid[x - 1][y]
-        right = grid[x][y - 1]
-        bottom = grid[x - 1][y]
-        left = grid[x][y + 1]
-    except:
-        return 0
+    grid_height = len(grid) - 1
+    grid_width = len(grid[0]) - 1
+
+    top = grid[x - 1][y] if x > 0 else 0
+    right = grid[x][y - 1] if y > 0 else 0
+    bottom = grid[x + 1][y] if x < grid_height else 0
+    left = grid[x][y + 1] if y < grid_width else 0
+
     exp_sides = 4 - top - right - bottom - left
     return exp_sides
 
@@ -29,29 +31,23 @@ def island_perimeter(grid):
         grid (2d array): grid map
     """
     if not grid:
-        return
-    
+        return 0
+
     grid_height = len(grid)
     grid_width = len(grid[0])
     perimeter = 0
+    island = 0
 
     if grid_height > 100 or grid_width > 100:
-        return
-    
+        return 0
+
     for i in range(grid_height):
         for j in range(grid_width):
             if grid[i][j] == 1:
                 exp_sides = find_exposed_sides((i, j), grid)
+                if island == 1 and exp_sides > 0:
+                    return 0
+                if exp_sides == 4:
+                    island += 1
                 perimeter += exp_sides
     return perimeter
-                
-  
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
